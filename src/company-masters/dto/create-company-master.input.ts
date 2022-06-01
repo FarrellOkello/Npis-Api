84b18a9151a0e1f17c551f04db8b895e89d9fa47
Farrel James Okello <farrel.okello@technobraingroup.com>
2022-06-01 14:40:07 +0300

    the nullable fieelds were added

--- src/company-masters/dto/create-company-master.input.ts
+++ src/company-masters/dto/create-company-master.input.ts
@@ -3,40 +3,40 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 @InputType()
 export class CreateCompanyMasterInput {
   @Field() readonly CompanyCode: string
-  @Field() readonly CompanyName: string
-  @Field() readonly CompanyRegistrationNumber: string
-  @Field() readonly Email: string
-  @Field() readonly AlternateEmail: string
-  @Field() readonly ContactPersonName: string
-  @Field() readonly ContactNumber: string
-  @Field() readonly AlternateContactNumber: string
-  @Field() readonly Fax: string
-  @Field() readonly TaxIdentificationNumber: string
-  @Field() readonly Nationality: string
-  @Field() readonly Corporatestatus: string
-  @Field() readonly IsSubmitted: number
-  @Field() readonly IsActive: number
-  @Field() readonly CompanyType: string;
-  @Field() readonly RegisterAddress: string;
-  @Field() readonly AlternateAddress: string;
-  @Field() readonly District: string;
-  @Field() readonly CompanyRegion: string;
-  @Field() readonly County: string;
-  @Field() readonly Subcounty: string;
-  @Field() readonly Parish: string;
-  @Field() readonly Zone_LC1: string;
-  @Field() readonly PostalCode: string;
-  @Field() readonly CreatedBy: number
-  @Field() readonly ModifiedBy: number
-  @Field() readonly CompanyRole: string;
-  @Field() readonly ParentID: string;
-  @Field() readonly CompanyLogo: string;
-  @Field() readonly CompanyLogoExtension: string;
-  @Field() readonly PermitType: string;
-  @Field() readonly PermitNumber: string;
-  @Field() readonly IssueDate: string;
-  @Field() readonly ExpiryDate: string;
-  @Field() readonly PermitStatus: string;
-  @Field() readonly Company_Site: string;
-  @Field() readonly PermitDetails: string;
+  @Field({ nullable: true }) readonly CompanyName: string
+  @Field({ nullable: true }) readonly CompanyRegistrationNumber: string
+  @Field({ nullable: true }) readonly Email: string
+  @Field({ nullable: true }) readonly AlternateEmail: string
+  @Field({ nullable: true }) readonly ContactPersonName: string
+  @Field({ nullable: true }) readonly ContactNumber: string
+  @Field({ nullable: true }) readonly AlternateContactNumber: string
+  @Field({ nullable: true }) readonly Fax: string
+  @Field({ nullable: true }) readonly TaxIdentificationNumber: string
+  @Field({ nullable: true }) readonly Nationality: string
+  @Field({ nullable: true }) readonly Corporatestatus: string
+  @Field({ nullable: true }) readonly IsSubmitted: number
+  @Field({ nullable: true }) readonly IsActive: number
+  @Field({ nullable: true }) readonly CompanyType: string;
+  @Field({ nullable: true }) readonly RegisterAddress: string;
+  @Field({ nullable: true }) readonly AlternateAddress: string;
+  @Field({ nullable: true }) readonly District: string;
+  @Field({ nullable: true }) readonly CompanyRegion: string;
+  @Field({ nullable: true }) readonly County: string;
+  @Field({ nullable: true }) readonly Subcounty: string;
+  @Field({ nullable: true }) readonly Parish: string;
+  @Field({ nullable: true }) readonly Zone_LC1: string;
+  @Field({ nullable: true }) readonly PostalCode: string;
+  @Field({ nullable: true }) readonly CreatedBy: number
+  @Field({ nullable: true }) readonly ModifiedBy: number
+  @Field({ nullable: true }) readonly CompanyRole: string;
+  @Field({ nullable: true }) readonly ParentID: string;
+  @Field({ nullable: true }) readonly CompanyLogo: string;
+  @Field({ nullable: true }) readonly CompanyLogoExtension: string;
+  @Field({ nullable: true }) readonly PermitType: string;
+  @Field({ nullable: true }) readonly PermitNumber: string;
+  @Field({ nullable: true }) readonly IssueDate: string;
+  @Field({ nullable: true }) readonly ExpiryDate: string;
+  @Field({ nullable: true }) readonly PermitStatus: string;
+  @Field({ nullable: true }) readonly Company_Site: string;
+  @Field({ nullable: true }) readonly PermitDetails: string;
 }
--- src/users/modals/User.ts
+++ src/users/modals/User.ts
@@ -1,5 +1,4 @@
 import { Field, Int, ObjectType } from "@nestjs/graphql";
-import { IsNotEmpty, IsOptional } from "class-validator";
 import { Entity } from "typeorm";
 
 @ObjectType()
